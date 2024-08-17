88c040db116694538da21ea2af4596603b1c067c
InesParis <inesparisarranz@gmail.com>
2024-08-17 22:20:28 +0200

    forecast display
    css display of forecast

--- src/ForecastData.js
+++ src/ForecastData.js
@@ -1,17 +1,16 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import "./ForecastData.css";
 export default function ForecastData({ forecast }) {
   return (
     <div className="forecast">
       <h3>5-Day Forecast</h3>
-      <div className="forecast-grid">
+      <div className="forecast-horizontal">
         {forecast.map((day, index) => (
           <div key={index} className="forecast-day">
             <FormattedDate date={new Date(day.time * 1000)} />{" "}
-            {/* Format the date */}
-            <WeatherIcon code={day.condition.icon} size={50} />{" "}
-            {/* Use WeatherIcon */}
+            <WeatherIcon code={day.condition.icon} size={50} />
             <div className="forecast-temperature">
               {Math.round(day.temperature.day)}°C
             </div>
